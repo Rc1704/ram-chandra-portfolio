43ef28c27f25e881a55870138eefa475a6c80801
pranav2885 <143580265+pranav2885@users.noreply.github.com>
2024-07-25 22:01:47 +0530

    commit

--- Portfolio/master/src/components/Navbar.js
+++ Portfolio/master/src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Rc1704/ram-chandra-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
--- Portfolio/master/src/components/Resume/ResumeNew.js
+++ Portfolio/master/src/components/Resume/ResumeNew.js
@@ -6,7 +6,7 @@ import pdf from "../../Assets/../Assets/RAM CHANDRA GUTTA CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import { FaDochub, FaFile } from "react-icons/fa";
+import { FaFile } from "react-icons/fa";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
